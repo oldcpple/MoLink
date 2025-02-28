bcc7cdd79d3f492dbf3ba6438cdfdbbc97eeb5d9
yqchen <978691801@qq.com>
2025-02-28 14:18:34 +0800

    Pipeline Parallelism and Micro-Batch implementation

--- molink/executor/mp_distributed_executor.py
+++ molink/executor/mp_distributed_executor.py
@@ -1,10 +1,13 @@
 import asyncio
-import grpc
 import time
 import torch
 import json
 import io
 import msgspec
+import grpc
+import copy
+import traceback
+from grpc import aio
 from concurrent import futures
 from typing import Any, Awaitable, Dict, List, Optional, Set, Tuple, Union
 from vllm.executor.mp_distributed_executor import MultiprocessingDistributedExecutor
@@ -14,8 +17,9 @@ from vllm.executor.multiproc_worker_utils import (
 from vllm.utils import (_run_task_with_lock, get_distributed_init_method,
                         get_ip, get_open_port, make_async)
 from vllm.config import VllmConfig
-from vllm.sequence import ExecuteModelRequest, SamplerOutput, IntermediateTensors
+from vllm.model_executor.layers.sampler import SamplerOutput
 from vllm.distributed import get_pp_group
+from vllm.sequence import IntermediateTensors, ExecuteModelRequest
 from molink.worker.worker_base import MolinkWorkerWrapperBase
 from molink.config import MolinkConfig
 from molink.dht.proto import comm_pb2, comm_pb2_grpc
@@ -49,12 +53,16 @@ class MolinkMultiprocessingDistributedExecutor(MultiprocessingDistributedExecuto
         self.pipeline_manager = None
         self.comm_handler = None
         self.grpc_server = None
+        self.preset_next_server = None
+        self.channel_to_next_server = None
 
         self._init_executor()
 
     def _init_executor(self) -> None:
         # Create the parallel GPU workers.
-        world_size = self.parallel_config.world_size
+
+        # for testing
+        world_size = self.parallel_config.tensor_parallel_size
         tensor_parallel_size = self.parallel_config.tensor_parallel_size
 
         # Set multiprocessing envs that are common to V0 and V1
@@ -131,148 +139,181 @@ class MolinkMultiprocessingDistributedExecutor(MultiprocessingDistributedExecuto
         end_layer = self.pipeline_config.serving_layers[1]
         self.dht_node = DHTNode(initial_peer, model_name, start_layer, end_layer)
         self.pipeline_manager = PipelineManager(self.dht_node)
-        self.comm_handler = CommService(self.pipeline_config.pipeline_parallel_size, self.driver_worker)
-        self.grpc_server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-        comm_pb2_grpc.add_ExampleServiceServicer_to_server(CommService(), self.grpc_server)
-        port = self.dht_node.node_info.get('grpc_port')
+        self.comm_handler = CommService(self.parallel_config.pipeline_parallel_size, self)
+        self.grpc_server = aio.server(futures.ThreadPoolExecutor(max_workers=10),
+                                            options=[
+                                                ('grpc.max_send_message_length', 200 * 1024 * 1024),  # 200MB
+                                                ('grpc.max_receive_message_length', 200 * 1024 * 1024)  # 200MB
+                                            ])
+        comm_pb2_grpc.add_CommServiceServicer_to_server(self.comm_handler, self.grpc_server)
+        port = self.dht_node.node_info.grpc_port
         self.grpc_server.add_insecure_port('[::]:{}'.format(port))
-        self.grpc_server.start()
-        self.grpc_server.wait_for_termination()
+        asyncio.create_task(self._start_grpc_server())
 
+        grpc_info = f'{self.dht_node.ip}:{self.dht_node.node_info.grpc_port}'
+        dht_info = f'{self.dht_node.ip}:{self.dht_node.node_info.dht_port}'
+
+        print("DISTRIBUTED SERVICE INFO: MoLink gRPC server works at {}, ".format(grpc_info))
+        print("DISTRIBUTED SERVICE INFO: MoLink DHT server works at {}".format(dht_info))
+        print("DISTRIBUTED SERVICE INFO: If this is the first node of the swarm, you can copy the DHT INFO as the initial peer of following nodes")
+
+
+
+    async def _start_grpc_server(self):
+        try:
+
+            await self.grpc_server.start()
+            await self.grpc_server.wait_for_termination()
+
+        except asyncio.CancelledError:
+            await self.grpc_server.stop(grace=5)
+
+    def _establish_conn_with_next_server(self, next_server):
+        # will be trigger during the ever first run
+        try:
+
+            if self.channel_to_next_server is not None:
+                del self.channel_to_next_server
+            self.channel_to_next_server = aio.insecure_channel(next_server,
+                                    options=[
+                                        ('grpc.max_send_message_length', 200 * 1024 * 1024),  # 200MB
+                                        ('grpc.max_receive_message_length', 200 * 1024 * 1024)  # 200MB
+                                    ])
+
+        except Exception as e:
+            print('Encounter the following exception: {}'.format(e))
+            traceback.print_exc()
+
+
+    async def execute_model_async(
+            self,
+            execute_model_req: ExecuteModelRequest) -> List[SamplerOutput]:
+        try:
+
+            if self.parallel_worker_tasks is None:
+                # Start model execution loop running in the parallel workers
+                self.parallel_worker_tasks = asyncio.create_task(
+                    self._start_worker_execution_loop())
+                
+        except Exception as e:
+            print('Encounter the following exception: {}'.format(e))
+            traceback.print_exc()
+
+        # Only the driver worker returns the sampling results.
+        return await self._driver_execute_model_async(execute_model_req)
 
     async def _driver_execute_model_async(
         self,
         execute_model_req: Optional[ExecuteModelRequest] = None,
     ) -> List[SamplerOutput]:
-        if self.pp_locks is None:
-            # This locks each pipeline parallel stage so multiple virtual
-            # engines can't execute on the same stage at the same time
-            # We create the locks here to avoid creating them in the constructor
-            # which uses a different asyncio loop.
-            self.pp_locks = [
-                asyncio.Lock()
-                for _ in range(self.parallel_config.pipeline_parallel_size)
+        try:
+            # bypass the empty execution
+            if execute_model_req is None:
+                return
+
+            if self.pp_locks is None:
+                # This locks each pipeline parallel stage so multiple virtual
+                # engines can't execute on the same stage at the same time
+                # We create the locks here to avoid creating them in the constructor
+                # which uses a different asyncio loop.
+                self.pp_locks = [
+                    asyncio.Lock()
+                    for _ in range(self.parallel_config.pipeline_parallel_size)
+                ]
+
+            grpc_metadata = self.pipeline_manager.pipeline_info
+            if len(grpc_metadata) <= 0:
+                server_list_raw = []
+            else:
+                server_list_raw = grpc_metadata.get('server_list')
+            # pop the head server
+            server_list = server_list_raw[1:]
+
+            tasks = [
+                asyncio.create_task(
+                    _run_task_with_lock(self.executing_head_server, self.pp_locks[0],
+                                        execute_model_req, grpc_metadata))
             ]
 
-        grpc_metadata = self.pipeline_manager.pipeline_info
-        server_list = grpc_metadata.get('server_list')
+            stub_list = [comm_pb2_grpc.CommServiceStub(aio.insecure_channel(server)) for server in server_list]
+            virtual_engine = execute_model_req.virtual_engine
 
-        tasks = [
-            asyncio.create_task(
-                _run_task_with_lock(self.executing_head_server, self.pp_locks[0],
-                                    execute_model_req, grpc_metadata))
-        ]
+            trigger_request = comm_pb2.GrpcTriggerRequest(virtual_engine = virtual_engine)
 
-        stub_list = [comm_pb2_grpc.CommServiceStub(grpc.insecure_channel(server)) for server in server_list]
-        virtual_engine = execute_model_req.virtual_engine
+            for pp_rank, stub in enumerate(stub_list,
+                                                    start=1):
+                tasks.append(
+                    asyncio.create_task(
+                        _run_task_with_lock(stub.ExecutingWorkerStep,
+                                            self.pp_locks[pp_rank],
+                                            trigger_request)))
+                
+            results = await self.comm_handler.output_queue[virtual_engine].get()
 
-        for pp_rank, stub in enumerate(stub_list,
-                                                start=1):
-            tasks.append(
-                asyncio.create_task(
-                    _run_task_with_lock(stub.executing_worker_step,
-                                        self.pp_locks[pp_rank],
-                                        virtual_engine)))
-            
-        results = await self.comm_handler.output_queue[virtual_engine].get()
-
-        # Only the last PP stage has the final results.
-        return results
-    
-    def execute_model(
-        self,
-        execute_model_req: Optional[ExecuteModelRequest] = None,
-        intermediate_tensors: Optional[IntermediateTensors] = None,
-    ) -> Optional[List[SamplerOutput]]:
-        """Executes at least one model step on the given sequences, unless no
-        sequences are provided."""
-        start_time = time.perf_counter()
-
-        inputs = self.prepare_input(execute_model_req)
-        if inputs is None:
-            return None
-
-        model_input, worker_input, kwargs = inputs
-        num_steps = worker_input.num_steps
-        if (execute_model_req is not None and execute_model_req.spec_step_idx):
-            kwargs["spec_step_idx"] = execute_model_req.spec_step_idx
-
-        self.execute_worker(worker_input)
-
-        # If there is no input, we don't need to execute the model.
-        if worker_input.num_seq_groups == 0:
-            return []
-
-        orig_model_execute_time = 0.0
-        if not get_pp_group().is_first_rank:
-            intermediate_tensors = None
-            if (self.observability_config is not None
-                    and self.observability_config.collect_model_execute_time):
-                orig_model_execute_time = intermediate_tensors.tensors.get(
-                    "model_execute_time", torch.tensor(0)).item()
-
-        output = self.model_runner.execute_model(
-            model_input=model_input,
-            kv_caches=self.kv_cache[worker_input.virtual_engine]
-            if self.kv_cache is not None else None,
-            intermediate_tensors=intermediate_tensors,
-            num_steps=num_steps,
-            **kwargs,
-        )
-
-        model_execute_time = time.perf_counter() - start_time
-        if not get_pp_group().is_last_rank:
-            # output is IntermediateTensors
-            assert isinstance(output, IntermediateTensors)
-            if (self.observability_config is not None
-                    and self.observability_config.collect_model_execute_time):
-                output.tensors["model_execute_time"] = torch.tensor(
-                    model_execute_time + orig_model_execute_time)
-
-        if (self.observability_config is not None
-                and self.observability_config.collect_model_execute_time
-                and output is not None):
-            for o in output:
-                o.model_execute_time = (orig_model_execute_time +
-                                        model_execute_time)
-
-        # output is List[SamplerOutput]
-        return output
+            return results
+        
+        except Exception as e:
+            print('Encounter the following exception: {}'.format(e))
+            traceback.print_exc()
     
     async def executing_head_server(self, execute_model_req: Optional[ExecuteModelRequest] = None, grpc_metadata: Optional[dict] = None):
 
-        virtual_engine = execute_model_req.virtual_engine
+        try:
+            virtual_engine = execute_model_req.virtual_engine
 
-        # if pipeline parallel size >= 2, it's IntermediateTensors
-        # otherwise it's [SamplerOutput]
-        outputs = await self.execute_model(execute_model_req)
+            # if pipeline parallel size >= 2, it's [IntermediateTensors.tensors]
+            # otherwise it's [SamplerOutput]
+            outputs = await self.driver_exec_model(execute_model_req)
 
-        server_list = grpc_metadata.get('server_list')
-        if len(server_list) <= 0:
-            self.comm_handler.output_queue[virtual_engine].put_nowait(outputs)
-            return
-        # ip : grpc_port
-        next_server = server_list[0]
-        server_list = server_list[1:]
-        grpc_metadata.update({'server_list' : server_list})
-        grpc_metadata = json.dumps(grpc_metadata).encode('utf-8')
-        execute_model_req.async_callback = None
-        bytes_emr = msgspec.json.encode(execute_model_req)
+            if len(grpc_metadata) <= 0:
+                server_list = []
+            else:
+                server_list = grpc_metadata.get('server_list')
+            # ip : grpc_port
 
-        intermediate_tensors = outputs
-        grpc_intermediate_tensors = comm_pb2.IntermediateTensors()
-        for key, tensors in intermediate_tensors.items():
-            buffer = io.BytesIO()
-            torch.save(tensors, buffer)
-            byte_data = buffer.getvalue()
-            grpc_intermediate_tensors.tensors.append(comm_pb2.TensorEntry(key = key,
-                                                                                tensor_data = byte_data))
+            if len(server_list) <= 1:
+                self.comm_handler.output_queue[virtual_engine].put_nowait(outputs)
+                return
+            
+            outputs = outputs[0]
+            next_server = server_list[1]
+            grpc_metadata = json.dumps(grpc_metadata).encode('utf-8')
+            execute_model_req.async_callback = None
 
-        grpc_request_data = comm_pb2.GrpcRequestData(execute_model_request = bytes_emr,
-                                                        intermediate_tensors = grpc_intermediate_tensors,
-                                                        grpc_metadata = grpc_metadata,
-                                                        virtual_engine = virtual_engine)
-        
-        with grpc.insecure_channel(next_server) as channel:
-            stub = comm_pb2_grpc.CommServiceStub(channel)
+            seq_data_dict = execute_model_req.seq_group_metadata_list[0].seq_data
+            for idx, seq_data in seq_data_dict.items():
+                seq_data._prompt_token_ids = list(seq_data._prompt_token_ids)
+                seq_data._output_token_ids = list(seq_data._output_token_ids)
+                seq_data_dict.update({idx : seq_data})
+
+            execute_model_req.seq_group_metadata_list[0].seq_data = seq_data_dict
+            #execute_model_req.seq_group_metadata_list = None
+            bytes_emr = msgspec.json.encode(execute_model_req)
+
+            intermediate_tensors = outputs
+            grpc_intermediate_tensors = comm_pb2.IntermediateTensors()
+            for key, tensors in intermediate_tensors.items():
+                buffer = io.BytesIO()
+                torch.save(tensors, buffer)
+                byte_data = buffer.getvalue()
+                grpc_intermediate_tensors.tensors.append(comm_pb2.TensorEntry(key = key,
+                                                                                    tensor_data = byte_data))
+
+            grpc_request_data = comm_pb2.GrpcRequestData(execute_model_request = bytes_emr,
+                                                            intermediate_tensors = grpc_intermediate_tensors,
+                                                            grpc_metadata = grpc_metadata,
+                                                            virtual_engine = virtual_engine)
+            
+            # case 1: first run
+            # case 2: the pipeline path has changed
+            if self.preset_next_server is None or self.preset_next_server != next_server:
+                self.preset_next_server = next_server
+                self._establish_conn_with_next_server(next_server)
+
+            assert self.channel_to_next_server is not None, 'Connection to next server has not been properly set'
+            stub = comm_pb2_grpc.CommServiceStub(self.channel_to_next_server)
             res = await stub.PushIntermediateTensors(grpc_request_data)
+
+        except Exception as e:
+            print('Encounter the following exception: {}'.format(e))
+            traceback.print_exc()
